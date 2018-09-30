bcdce25277d7438d865c8a6609bfc69edc437ed5
Phebe S <phebesawan@gmail.com>
2018-10-01 03:42:24 +0800

    task1: update jestSetup, add HomePage tests and snapshots and update Place snapshot.

---
+++ test/client/components/HomePage.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from 'enzyme';
+import { HomePage } from 'client/containers/HomePage';
+import renderer from 'react-test-renderer';
+
+jest.mock('components/Place/Place', () => 'Place');
+
+test('With Enzyme, HomePage component disables out the search Button when no latitude is fetched', () => {
+  const condition = { radius: 500 };
+  const wrapper = render(
+    <HomePage condition={condition} />,
+  );
+  const disabledButton = wrapper.find('.homepageClick.disabled');
+  expect(disabledButton.length).toBe(1);
+});
+
+test('With Jest snapshot, HomePage component disables and greys out the search Button when no latitude is fetched', () => {
+  const condition = { radius: 500 };
+  const homePageComponent = renderer
+    .create(<HomePage condition={condition} />)
+    .toJSON();
+  expect(homePageComponent).toMatchSnapshot();
+});
+
+test('With Jest snapshot, HomePage component enables the search Button when a latitude is fetched', () => {
+  const condition = { radius: 500, latitude: 7.1 };
+  const homePageComponent = renderer
+    .create(<HomePage condition={condition} />)
+    .toJSON();
+  expect(homePageComponent).toMatchSnapshot();
+});
